2e05a45e39b767f2dde70771eaff1076c94cc3c9
Jordan Crawford-O'Banner <jobanner@olin.edu>
2020-12-05 19:09:10 -0500

    Added Decoder function using tuples instead of 3d trellis

--- Decoder.py
+++ Decoder.py
@@ -33,33 +33,36 @@ def decoder(rxmsg,stmchn):
     ns = len(stmchn) # num of trellis rows, which is num states bc state machine is list of lists
     r = len(stmchn[0][0]) # num of parity bits
     tl = int((len(rxmsg)/r)+1) # num of trellis columns, which is the number of parity bits is any given window
-    trellis = np.zeros([tl,ns,2],dtype=int) # trellis with ns rows, tl columns, and tuple values
+    trellisham = np.zeros([ns,tl],dtype=object) # trellis with ns rows, tl columns, and tuple values
+    trellisbam = np.zeros([ns,tl],dtype=object) # trellis with ns rows, tl columns, and tuple values
     # Initialize first column in trellis
-    trellis[:,0,0]=np.inf
-    trellis[0,0,0]=0
+    trellisham[:,:]=np.inf
+    trellisham[0,0]=(0,"nan")
     # Iterate and update the rest of the trellis
     # for sym in rxmsg[::len(stmchn[0][0])]: # only gives the first element, not the whole section
     count = 0
+    print("initial trellis")
+    print(trellisham[:,:])
     for sym in range(0,len(rxmsg),r):
         count = count + 1 # what message portion are we on (start from 1)
         msg_section = rxmsg[sym:sym+r+1]
         for state in range(ns):
             s = bin(state)[2:] # extract the binary representation of the state
             p_as = s[1:] # predecessor state after shift, before adding in x[n]
-            min_dp = [0,0] # stores minimum hamming distance and predecessor state
+            min_dp = (np.inf,0) # stores minimum hamming distance and predecessor state
             for p in [p_as+'1',p_as+'0']:
                 p_dec = int(p,2) # decimal representation
                 x_n = s[0] # the value of x[n] that shifts from predecessor state to current state
                 ti = stmchn[p_dec][int(x_n)] # parity bits generated during transition
                 di = min_ham(msg_section, ti) # cost (hamming distance)
                 if di < min_dp[0]: # Check if cost is smaller than saved minimum
-                    min_dp = [di,p] # Update minimum cost and according state
-            trellis[state,count] = min_dp # Save minimum cost and according predecessor to trellis
+                    min_dp = (di,p) # Update minimum cost and according state
+            trellisham[state,count] = min_dp # Save minimum cost and according predecessor to trellis
 
     # Find the total minimum cost of the most likely path
-    min_cost = np.min(trellis[:,-1,0])
+    min_cost = np.min(trellisham[:,-1])
     # Find the state associated with the most likely path
-    end_state = np.argmin(trellis[:,-1,0])
+    end_state = np.argmin(trellisham[:,-1])
 
     # Backtrack and find the most likely sequence of states
     # Front-end insertion because backtracking (finds the last bit, first)
@@ -69,12 +72,15 @@ def decoder(rxmsg,stmchn):
     preds = collections.deque()
     preds.append(end_state) # add the state to start backtracking from
     # Begin backtracking
-    for bt_idx in np.flip(range(tl)):
+    for bt_idx in np.flip(range(tl),0):
         most_likely_states.appendleft(preds[-1])
-        preds.append(trellis[preds[-1],bt_idx,1])
+        preds.append(trellisbam[preds[-1],bt_idx])
 
     most_likely_states = list(most_likely_states)
-    return [b[0] for b in most_likely_states[1:]]
+    print(most_likely_states)
+    print("final trellis")
+    print(trellisham[:,:])
+    return [b for b in most_likely_states[1:]]
 
 def min_ham(rx,trial):
     dist = 0;
@@ -95,9 +101,103 @@ def state_machine_gen(K):
         stm[k]=parity_bits(s)
     return stm
 
+def thedecoder_part2_tenyearslater(rxmsg,stmchn):
+    """
+    State machine 2d = row state column state value tuple (transmitted bit, received bit)
+    Trellis values = 2d array
+            row is state
+            column is bit
+            value is a tuple of hamming distance for most likely  bit, and most likely prior state
+
+
+    Given that you’ve received L bits, and that each message bit encodes R parity bits:
+
+    From 1 -> L/R (iterate variable called i):
+    ri = received message bits for i-th segment
+    For all states si:
+        For all predecessor states pi:
+            ti = parity bits for transition.
+            di = Hamming distance between ri and ti
+        dmin = min(all di)
+        pmin = min(predecessor state that got dmin)
+        Store in Trellis values dmin for position (si,i)
+        Store in Trellis prior states pmin for position (si,i)
+
+    At the very end, find the send where dmin is the smallest. That’s the ending state.
+    From there, trace backwards what pmin was to the very beginning.
+
+    rxmsg - received message
+    stmchn - state machine:
+        array:
+            rows = all possible states
+            columns = message bit value (0 or 1)
+            value = string of parity bits
+    """
+    ### Initialize Trellis
+    ns = len(stmchn) # num of trellis rows, which is num states bc state machine is list of lists
+    r = len(stmchn[0][0]) # num of parity bits
+    tl = int((len(rxmsg)/r)+1) # num of trellis columns, which is the number of parity bits is any given window
+    trellisham = np.zeros([ns,tl],dtype=object) # trellis with ns rows, tl columns, and tuple values
+
+    # Initialize first column in trellis
+    trellisham[:,:]=np.inf
+    trellisham[0,0]=(0,"nan")
+    trellisham[1,0]=(np.inf,"nan")
+    trellisham[2,0]=(np.inf,"nan")
+    trellisham[3,0]=(np.inf,"nan")
+    count = 0
+    print("initial trellis")
+    print(trellisham[:,:])
+
+    ### filling the trellis
+    for sym in range(0,len(rxmsg),r):
+        count = count + 1 # what message portion are we on (start from 1)
+        msg_section = rxmsg[sym:sym+r+1]
+        for state in range(ns):
+            s = bin(state)[2:].zfill(2) # extract the binary representation of the state
+            p_as = s[1:] # predecessor state after shift, before adding in x[n]
+            min_dp = (np.inf,0) # stores minimum hamming distance and predecessor state
+            for p in [p_as+'1',p_as+'0']:
+                #print("pee",p)
+                p_dec = int(p,2) # decimal representation
+                x_n = s[0] # the value of x[n] that shifts from predecessor state to current state
+                ti = stmchn[p_dec][int(x_n)] # parity bits generated during transition
+                di = min_ham(msg_section, ti) # cost (hamming distance)
+                if di < min_dp[0]: # Check if cost is smaller than saved minimum
+                    min_dp = (di,p) # Update minimum cost and according state
+            trellisham[state,count] = min_dp # Save minimum cost and according predecessor to trellis
+
+    ### Backtracking
+
+    # Find the total minimum cost of the most likely path
+
+    min_cost = np.min(trellisham[:,-1])
+
+    # Find the state associated with the most likely path
+    end_state = np.argmin(trellisham[:,-1][1])
+    print("final trellis")
+    print(trellisham[:,:])
+
+    # Backtrack and find the most likely sequence of states
+    # Front-end insertion because backtracking (finds the last bit, first)
+    # Use a deque because front-end insertion
+    most_likely_states = collections.deque()
+    # Use a second deque to keep track of predecssor states
+    preds = collections.deque()
+    preds.append(end_state) # add the state to start backtracking from
+    # Begin backtracking
+    for bt_idx in np.flip(range(tl),0):
+        most_likely_states.appendleft(preds[-1])
+        preds.append(trellisam[preds[-1],bt_idx])
+
+    most_likely_states = list(most_likely_states)
+
+    return [b for b in most_likely_states[1:]]
+
 if __name__ == "__main__":
     print("Testing decoding.")
     test =  "111101000110"
     K = 3; # Constraint length
     state_machine = state_machine_gen(K-1);
-    decoded = decoder(test,state_machine)
+    decoded = thedecoder_part2_tenyearslater(test,state_machine)
+    print(decoded)
